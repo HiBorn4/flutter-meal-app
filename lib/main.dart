b31f667510a2bbea3f1c0181de1788cb9c41d602
hiborn4 <hiborn4@gmail.com>
2023-06-20 16:36:20 -0500

    Added Screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -80,6 +80,22 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+  final customPrimaryColor = const MaterialColor(
+    0xFF000000, // Replace with your desired primary color value
+    <int, Color>{
+      50: Color(0xFF000000),
+      100: Color(0xFF000000),
+      200: Color(0xFF000000),
+      300: Color(0xFF000000),
+      400: Color(0xFF000000),
+      500: Color(0xFF000000),
+      600: Color(0xFF000000),
+      700: Color(0xFF000000),
+      800: Color(0xFF000000),
+      900: Color(0xFF000000),
+    },
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -99,8 +115,11 @@ class _MyAppState extends State<MyApp> {
               fontFamily: 'RobotoCondensed',
               fontWeight: FontWeight.bold,
             )),
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
-            .copyWith(secondary: Colors.amber),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: customPrimaryColor,
+        ).copyWith(
+          secondary: Colors.grey,
+        ),
       ),
       home: TabsScreen(_favouriteMeals),
       routes: {
